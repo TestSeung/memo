f757faec22af9a3820ec8b0cf17fb69e2ea7c80d
TestSeung <sjlim589@hanmail.net>
2024-08-09 20:58:40 +0900

    Fast Api memo

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+class Memo(BaseModel):
+    id:int
+    content:str
+    
+memos =[]
+
+app = FastAPI()
+
+@app.post("/memos")
+def create_memo(memo:Memo):
+    memos.append(memo)
+    return "추가완료"
+
+
+@app.get("/memos")
+def read_memo():
+    return memos
+
+app.mount("/",StaticFiles(directory='static',html=True),name= 'static')
