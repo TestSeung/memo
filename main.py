40e5a31f9abc689e5382580afd2cd61a918d4322
TestSeung <sjlim589@hanmail.net>
2024-08-12 21:53:44 +0900

    Memo

--- main.py
+++ main.py
@@ -20,4 +20,20 @@ def create_memo(memo:Memo):
 def read_memo():
     return memos
 
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id==req_memo.id:
+            memo.content = req_memo.content
+            return '성공했습니다.'
+    return '그런 메모는 없습니다.'
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id:int): #매개변수 유형 지정
+    for index,memo in enumerate(memos): #배열의 index값을 같이 뽑아줌
+        if memo.id==memo_id:
+            memos.pop(index)
+            return '성공했습니다.'
+    return '그런 메모는 없습니다.'
+
 app.mount("/",StaticFiles(directory='static',html=True),name= 'static')
--- static/app.js
+++ static/app.js
@@ -1,7 +1,47 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  //console.log(event.target.dataset.id);
+  const editInput = prompt("수정할 값을 입력하세요"); //값을 입력할수있음
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT", //값 수정
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      //통신은 문자열만 가능하다
+      id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE", //값 삭제
+  });
+  readMemo();
+}
+
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
+
   const li = document.createElement("li");
   li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id; //메모아이디 버튼아이디로 설정
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
   ul.appendChild(li);
 }
 
